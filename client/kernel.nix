53156f5a05c5d66764251d069a68cb6b7e321369
Ewan Arends <ewan.arends10@gmail.com>
2025-12-19 15:52:44 +0100

    Try and find out why kernel stays the same

--- client/kernel.nix
+++ client/kernel.nix
@@ -1,9 +1,10 @@
 {
   pkgs, 
-    opts ? {} 
+    opts ? {
+      overrides = {};
+      } 
 }:
 let
  inherit (pkgs) lib;
 in
-pkgs.linuxPackages_custom_tinyconfig_kernel.override {
-} // (opts.overrides or {})
+pkgs.linuxPackages_custom_tinyconfig_kernel.override (opts.overrides)
